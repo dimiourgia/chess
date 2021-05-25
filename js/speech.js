a4ca0f227998507ba411691f6140b264c7eecdb7
dimiourgia <ajayaxes318@gmail.com>
2021-05-25 22:03:57 +0530

    Add files via upload

--- js/speech.js
+++ js/speech.js
@@ -1,66 +1,55 @@
 
-var speeds = {
-    pause:500,
-    slow:120,
-    normal:60,
-    fast:30,
-}  
+let move=1;
 
-  var dialogue= [
-    { 
-        string:'Hello Everyone!',
-        delay:speeds.slow,
-    },
-    { 
-        string:'Agadamator Here',
-        delay:speeds.normal,
-    },
-    { 
-        string:'Are you readay to solve some puzzles?',
-        delay:speeds.normal,
-    },
-]
-
-displayDialogue(dialogue);
+function formSubmit(){
+    let levels = document.querySelector('#levels').value;;
+    let fen = document.querySelector('#fen').value;
+    let black = document.querySelector('#black').value;
+    let white = document.querySelector('#white').value;
+    let intro = document.querySelector('#intro').value; 
+    let event = document.querySelector('#event').value;
 
 
-function displayDialogue(dialogue){
-    var speeds = {
-        pause:500,
-        slow:120,
-        normal:60,
-        fast:30,
-    }  
-    var characters=[];
-    var container = document.createElement('div');
-    container.classList.add('dialogue');
-    document.body.appendChild(container);
-    dialogue.forEach((line,index) =>{
-        if(index< dialogue.length) line.string += " ";
-    
-        line.string.split("").forEach(c=>{
-            var span = document.createElement('span');
-            span.textContent = c;
-            container.append(span);
-            characters.push({
-                span: span,
-                delay: line.delay,
-                classList: line.classes || [],
-            });    
-        })
-    });
+    console.log(event,levels,fen,black,white,intro);
 
-    revealCharacter(characters);
+    return false;
+}
+
+function addMove(){
+    move++;
+    let table = document.querySelector('#moves_form');
+    var row1 = table.insertRow();
+    var row2 = table.insertRow();
+
+    cell1 = row1.insertCell();
+    cell2 = row1.insertCell();
+    cell3 = row1.insertCell();
+    cell4 = row1.insertCell();
+    cell5 = row2.insertCell();
+
+    cell1.innerHTML = "<input type='text' name='move' placeholder='move' id='move" + move +  "'>";
+    cell2.innerHTML =  "<input type='text' name='response' placeholder='response' id='move" + move +  "_response'>";
+    cell3.innerHTML = "<label for='correct'>correct</label><input type='checkbox' name='correct' id='move" + move +  "_correct'>";
+    cell4.innerHTML =  "<label for='correct'>final</label><input type='checkbox' name='final' id='move" + move +  "_final'>";
+    cell5.innerHTML = "<input type='text' name='dialogue' placeholder='dialogue' id='move" + move +  "_dialogue'>";
+    cell5.setAttribute('colspan',4);
+    document.querySelector('moves_form');
 }
 
 
-function revealCharacter(list){
-    var next = list.splice(0,1)[0];
-    next.span.classList.add('revealed');
-    if(list.length>0){
-        setTimeout(function(){
-            revealCharacter(list);
-        },next.delay);
-    }
-    
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {edge: 'left'});
+  });
+
+
+  function close_settings(){
+    tmp=document.querySelector('.settings').style.display = 'none';
+}
+
+
+function show_settings(){
+    tmp=document.querySelector('.settings').style.display = 'block';
 }
